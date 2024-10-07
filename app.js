b0d7c68ce421ed59140188bc08ab3a7676ef85c5
MAsood Al Razzaq <masood.al.razzaq@gmail.com>
2024-10-07 21:47:01 +0530

    app.js added

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
-// js file added 
+console.log("hello")
+settimeout(()=>{
+    console.log("inside settimeout")
+},0)
+let a=[1,2,3,4,5]
+let p=new Promise((resolve,reject)=>{
+    if(a.includes(2)){
+        resolve("problem resolve")
+    }
+    else{
+        reject("rejected")
+    }
+})
+
+p.then((data)=>{
+    console.log(data)
+}).catch((data1)=>{
+    console.log(data1)
+})
+    
